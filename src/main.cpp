ec880574f06108d7f129e40f7a3f1204d9c6206c
Salvador Guzman <guzmansalv@gmail.com>
2018-05-02 16:21:42 -0700

    simple redis example

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
+#include <cpp_redis/cpp_redis>
 #include "http-get.hxx"
 
 constexpr static const char * const host{"23.95.221.108"};
 
 int main() {
-  const auto& html{HTTP::get(host)};
-  std::cout << html.length() << std::endl;
-  std::cout << html << std::endl;
+  cpp_redis::client client;
+  client.connect();
+
+  client.set("hello", "42");
+  {
+    client.sync_commit();
+  }
+
+  auto helloFtr{client.get("hello")};
+  {
+    client.sync_commit();
+  }
+  std::cout << helloFtr.get() << std::endl;
 
   return EXIT_SUCCESS;
 }
