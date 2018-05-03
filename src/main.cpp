2462fe323d7ec49c0fc4b0ed4b5af55c821e5268
Salvador Guzman <guzmansalv@gmail.com>
2018-05-03 14:02:20 -0700

    do not need to sync

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 constexpr static const unsigned short limit = 1276;
 
 int main() {
+  std::__1::ostream::sync_with_stdio(false);
   Redis::redis_init();
 
   for (unsigned short i{1u}; i < limit; ++i) {
